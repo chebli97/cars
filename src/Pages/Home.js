49232a9d3f3c803a0d6a594abb4983a56e873996
unknown <malekjouini47@gmail.com>
2021-07-10 02:15:03 +0100

    10/07/2021 02:15

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,8 +6,12 @@ function Home() {
     return (
         <div>
             <h1 className="title-of-box">Browse our latest cars</h1>
+            <img className="flech" src="https://img.icons8.com/ios-filled/50/000000/circled-chevron-left.png" />
+            <img className="flech1" src="https://img.icons8.com/ios-filled/50/000000/circled-chevron-right.png" />
             <div className="box-cars-home">
                 <Article Car={Car} />
+                <div className="d-grid gap-2 col-6 mx-auto"></div>
+                <a href="#" className="btn btn-secondary">search</a>
             </div>
         </div>
     )
--- src/components/Article.js
+++ src/components/Article.js
@@ -2,16 +2,23 @@ import React from 'react';
 
 function Article({ Car }) {
     return (
+        
         <>
-
+ 
             {
                 Car.map(i => (
+                    
                     <div className="container">
                         <section>
                             <img className="image-box" src={i.image} />
                             <h1 className="name-style"> {i.marque} {i.model}</h1>
-                            <h3 className="detail-car">{i.cylindrée} {i.carburant}</h3>
-                            <h2 className>{i.carburant} </h2><h2>{i.année}</h2>
+                            <h3 className="detail-car">{i.cylindrée} {i.carburant} {i.année}</h3>
+                            <h3 className="km">{i.kilométrage}</h3>
+                            <h2 className="prix">{i.prix} </h2>
+                            <div className="d-grid gap-2 col-6 mx-auto">
+                                <a className="btn btn-outline-danger">View the car</a>
+                                <h6>Added 6 hours ago</h6>
+                            </div>
                         </section>
                     </div>
                 ))
